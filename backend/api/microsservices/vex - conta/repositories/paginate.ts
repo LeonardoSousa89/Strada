64628ef2fc63a32e3a046277b930d7499ae294d6
leonardoSousa89 <leoprofessionallogytech@outlook.com>
2023-08-18 00:11:13 -0300

    @v1.2.5 -   implementação de anotação da expressão matemática em paginate.ts

--- backend/api/microsservices/vex - conta/repositories/paginate.ts
+++ backend/api/microsservices/vex - conta/repositories/paginate.ts
@@ -1,3 +1,16 @@
+/**
+ *  referência: 
+ *  https://www.youtube.com/watch?v=avUCPbWppgQ
+ */
+
+/**
+  expressão matemática para cálculo da paginação
+  
+  x = page 
+  y = size
+  
+  x > 1 | x = (x * y) - y
+**/ 
 export default function calculatePage(page: any, size: any) {
   page = Number(page);
 
--- backend/api/microsservices/vex - conta/src/repositories/paginate.js
+++ backend/api/microsservices/vex - conta/src/repositories/paginate.js
@@ -1,5 +1,17 @@
 "use strict";
+/**
+ *  referência:
+ *  https://www.youtube.com/watch?v=avUCPbWppgQ
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
+/**
+  expressão matemática para cálculo da paginação
+  
+  x = page
+  y = size
+  
+  x > 1 | x = (x * y) - y
+**/
 function calculatePage(page, size) {
     page = Number(page);
     if (page < 0)
