42ad700d405ae4ff8fab6e73e5d37a34a8ffb20e
leonardoSousa89 <leoprofessionallogytech@outlook.com>
2023-09-17 18:47:55 -0300

    @v1.2.25 - criação da tela de boas vindas a organização

--- mobile/Vex/src/modules/checklist/screens/welcome/welcomeOrganization.tsx
+++ mobile/Vex/src/modules/checklist/screens/welcome/welcomeOrganization.tsx
@@ -1,17 +1,71 @@
-import { StyleSheet, View, Image, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Button } from "@react-native-material/core";
+import { StyleSheet, View, Image, Text, Dimensions } from "react-native";
 
-export default(props: any) => {
-  
+export default (props: any) => {
+  const [org, setOrg]: Array<any> = useState();
+
+  useEffect(() => setOrg("nome da organização"), []);
+
   return (
     <View style={styles.container}>
-      <Text>welcome organization screen</Text>
+      <View style={styles.icon}>
+        <Image
+          source={require("../../../../assets/global/circular_logo.png")}
+        />
+      </View>
+
+      <View style={styles.info}>
+        <Text style={styles.paragraph}>Organização</Text>
+        <Text style={styles.paragraphParams}>{org}</Text>
+      </View>
+
+      <View style={styles.operation}>
+        <Button
+          style={styles.btn}
+          title="prosseguir"
+          tintColor="#9BAEBF"
+          color="#2B3640"
+          // onPress={() => login()}
+        />
+      </View>
     </View>
   );
-}
+};
+
+const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  paragraph: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#2B3640",
+    marginBottom: 5,
+  },
+  paragraphParams: {
+    fontSize: 22,
+    color: "#2B3640",
+  },
+  btn: {},
+  icon: {
+    flex: 2,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  info: {
+    flex: 2,
+    width,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  operation: {
+    flex: 1,
+    width: "60%",
   },
 });
