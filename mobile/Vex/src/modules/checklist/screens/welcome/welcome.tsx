1073743a8d5ebe60d5b593f1301c5b6c1113cc34
leonardoSousa89 <leoprofessionallogytech@outlook.com>
2023-09-18 00:49:22 -0300

    @v1.2.29 - atualização do design de welcome.tsx

--- mobile/Vex/src/modules/checklist/screens/welcome/welcome.tsx
+++ mobile/Vex/src/modules/checklist/screens/welcome/welcome.tsx
@@ -14,13 +14,6 @@ export default(props: any) => {
           source={require("../../../../assets/global/name_logo_and_registry.png")}
         />
       </View>
-
-      <View style={styles.field_down}>
-        <Image
-          style={styles.animation_image}
-          source={require("../../../../assets/screens/animation/animation_logo.png")}
-        />
-      </View>
     </SafeAreaView>
   );
 }
@@ -28,21 +21,13 @@ export default(props: any) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#162226",
   },
   field_right:{
-    flex: 3,
-    // backgroundColor: '#000', //para testes de layout
+    flex: 1,
     width: '100%',
     alignItems: "center",
-    justifyContent: "flex-end",
-  },
-  field_down:{
-    flex: 2,
-    // backgroundColor: '#038C7F',  //para testes de layout
-    width: '100%',
     justifyContent: "center",
-    alignItems: "center",
   },
   image: {},
   animation_image: {},
