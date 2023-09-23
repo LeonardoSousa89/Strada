50f75cd1b920e8bc27cd718261075877c25feaeb
leonardoSousa89 <leoprofessionallogytech@outlook.com>
2023-09-23 20:18:15 -0300

    @v1.2.13 - estilização dos ícones do route bottom tab do módulo de checklist

--- mobile/Vex/src/routes/tabs.tsx
+++ mobile/Vex/src/routes/tabs.tsx
@@ -4,6 +4,8 @@ import { SafeAreaView, StyleSheet } from "react-native";
 import ChecklistForm from "../modules/checklist/screens/checklist/checklistForm";
 import ChecklistView from "../modules/checklist/screens/checklist/routes/checklistDriverRouteList";
 
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
+
 const Tab = createBottomTabNavigator();
 
 export default function Route() {
@@ -11,20 +13,66 @@ export default function Route() {
     <SafeAreaView style={styles.container}>
       <Tab.Navigator
         initialRouteName="checklist-form"
-        screenOptions={{ headerShown: false }}
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: "#2B3640",
+          tabBarLabelStyle: { fontSize: 12 },
+          tabBarStyle: {
+            backgroundColor: "#fff",
+            height: 50,
+          },
+        }}
       >
         <Tab.Screen
           name="checklist-form"
           component={ChecklistForm}
           options={{
-            title: "",
+            title: "enviar checklist",
+            tabBarIcon: ({ focused, color, size }) => {
+              if (focused) {
+                return (
+                  <MaterialCommunityIcons
+                    name="archive-edit"
+                    size={30}
+                    color={"#2B3640"}
+                  />
+                );
+              }
+
+              return (
+                <MaterialCommunityIcons
+                  name="archive-edit-outline"
+                  size={30}
+                  color={"#9BAEBF"}
+                />
+              );
+            },
           }}
         />
         <Tab.Screen
           name="checklist-view"
           component={ChecklistView}
           options={{
-            title: "",
+            title: "visualizar checklist",
+            tabBarIcon: ({ focused, color, size }) => {
+              if (focused) {
+                return (
+                  <MaterialCommunityIcons
+                    name="book-check"
+                    size={30}
+                    color={"#2B3640"}
+                  />
+                );
+              }
+
+              return (
+                <MaterialCommunityIcons
+                  name="book-check-outline"
+                  size={30}
+                  color={"#9BAEBF"}
+                />
+              );
+            },
           }}
         />
       </Tab.Navigator>
