c45c518d804cb8bf8f77929ba59ffe74ee910f43
leonardoSousa89 <leoprofessionallogytech@outlook.com>
2023-10-01 21:11:55 -0300

    @v1.2.33 - estilização e operações básicas de teste nas telas de edição da conta: endereço e contato

--- mobile/Vex/src/modules/admin/screens/account/accountEditAddress.tsx
+++ mobile/Vex/src/modules/admin/screens/account/accountEditAddress.tsx
@@ -1,31 +1,129 @@
+import { Button, TextInput } from "@react-native-material/core";
+import React, { useState, useEffect } from "react";
+
 import {
-    Dimensions,
-    StyleSheet,
-    View,
-    Text,
-    Pressable,
-    Image,
-    SafeAreaView,
-  } from "react-native";
-  
-  export default (props: any) => {
-    return (
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  Dimensions,
+  Pressable,
+  SafeAreaView,
+  FlatList,
+  ScrollView,
+} from "react-native";
+
+export default (props: any) => {
+  return (
+    <ScrollView>
       <SafeAreaView style={styles.container}>
-        <View>
-         <Text>edição de endereço</Text>
+        <View style={styles.subcontainer}>
+          <TextInput
+            variant="outlined"
+            label="cep"
+            color="#162226"
+            style={styles.subcontainer_first_child}
+          />
+          <TextInput
+            variant="outlined"
+            label="tipo de logradouro"
+            color="#162226"
+            style={styles.input}
+          />
+          <TextInput
+            variant="outlined"
+            label="logradouro"
+            color="#162226"
+            style={styles.input}
+          />
+          <TextInput
+            variant="outlined"
+            label="número"
+            color="#162226"
+            style={styles.input}
+          />
+          <TextInput
+            variant="outlined"
+            label="complemento"
+            color="#162226"
+            style={styles.input}
+          />
+          <TextInput
+            variant="outlined"
+            label="bairro"
+            color="#162226"
+            style={styles.input}
+          />
+          <TextInput
+            variant="outlined"
+            label="munícipio"
+            color="#162226"
+            style={styles.input}
+          />
+          <TextInput
+            variant="outlined"
+            label="país"
+            color="#162226"
+            style={styles.input}
+          />
+          <Button
+            style={styles.btn}
+            title="Atualizar"
+            color="#162226"
+            onPress={() => console.warn("atualizar")}
+          />
         </View>
       </SafeAreaView>
-    );
-  };
-  
-  const { width, height } = Dimensions.get("window");
-  
-  const styles = StyleSheet.create({
+    </ScrollView>
+  );
+};
+
+const { width, height } = Dimensions.get("window");
+
+let styles: any = 0;
+
+if (height > 720) {
+  styles = StyleSheet.create({
     container: {
-      flex: 1,
+      height,
       backgroundColor: "#fff",
       justifyContent: "center",
       alignItems: "center",
     },
+    subcontainer: {
+      flex: 1,
+      width: "70%",
+    },
+    subcontainer_first_child: {
+      marginTop: 50,
+    },
+    input: {
+      marginTop: 10,
+    },
+    btn: {
+      marginTop: 20,
+    },
   });
-  
+} else {
+  styles = StyleSheet.create({
+    container: {
+      height: height + 50,
+      backgroundColor: "#fff",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    subcontainer: {
+      flex: 1,
+      width: "70%",
+    },
+    subcontainer_first_child: {
+      marginTop: 50,
+    },
+    input: {
+      marginTop: 10,
+    },
+    btn: {
+      marginTop: 20,
+    },
+  });
+}
--- mobile/Vex/src/modules/admin/screens/account/accountEditContact.tsx
+++ mobile/Vex/src/modules/admin/screens/account/accountEditContact.tsx
@@ -1,31 +1,70 @@
+import { Button, TextInput } from "@react-native-material/core";
+import React, { useState, useEffect } from "react";
+
 import {
-    Dimensions,
-    StyleSheet,
-    View,
-    Text,
-    Pressable,
-    Image,
-    SafeAreaView,
-  } from "react-native";
-  
-  export default (props: any) => {
-    return (
-      <SafeAreaView style={styles.container}>
-        <View>
-         <Text>edição de contato</Text>
-        </View>
-      </SafeAreaView>
-    );
-  };
-  
-  const { width, height } = Dimensions.get("window");
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-  });
-  
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  Dimensions,
+  Pressable,
+  SafeAreaView,
+  FlatList,
+} from "react-native";
+
+export default (props: any) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.subcontainer}>
+        <TextInput
+          variant="outlined"
+          label="ddd"
+          color="#162226"
+          style={styles.subcontainer_first_child}
+        />
+        <TextInput
+          variant="outlined"
+          label="telefone"
+          color="#162226"
+          style={styles.input}
+        />
+        <TextInput
+          variant="outlined"
+          label="email"
+          color="#162226"
+          style={styles.input}
+        />
+        <Button
+          style={styles.btn}
+          title="Atualizar"
+          color="#162226"
+          onPress={() => console.warn("atualizar")}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const { width, height } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  subcontainer: {
+    flex: 1,
+    width: "70%",
+  },
+  subcontainer_first_child:{
+    marginTop: 50
+  },
+  input: {
+    marginTop: 10
+  },
+  btn: {
+    marginTop: 20,
+  },
+});
